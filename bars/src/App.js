8157aced4a45fb72463c847d2c401461a1627a63
Shivani Soni <sonishivani505@gmail.com>
2022-07-13 23:15:21 +0530

    Adding top and side bar

---
+++ bars/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+   
+      <button>HOME</button>
+      <button>ABOUT US</button>
+      <button>REVIEWS</button>
+      <button>BRANCHES</button>
+      <button>CONTACT US</button>
+        <div className="side">
+          <li><a href="#">Our Story</a></li>
+          <li>Working Staff</li>
+          <li>Pictures</li>
+          <li>Achievements Done</li>
+          <li>Awards Recieved</li>
+          <li>Address</li>
+          <li><a href="#">Terms ans Cond.</a></li>
+
+
+        </div>
+
+    </div>
+   
+  );
+}
+
+export default App;
